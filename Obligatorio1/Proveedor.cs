468825312513c5fe80508b435f84a448c9e3a150
Richard Blank Pucholo <RB304390@fi365.ort.edu.uy>
2023-04-15 18:47:30 -0300

    Clases huesped y porveedor completas

--- Obligatorio1/Proveedor.cs
+++ Obligatorio1/Proveedor.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Obligatorio1
 {
-    internal class Proveedor
+    public class Proveedor
     {
+        public static int UltimoId { get; set; }
+        public int Id { get; set; }
+        public static string NombreUnico { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public int Descuento { get; set; }
+
+        public void EsVAlido()
+        { 
+        }
+
+        public bool FijarDescuentoActividad(int Descuento)
+        {
+            return false;
+        }
+
+        public Proveedor()
+        {
+            Id = UltimoId;
+            UltimoId++;
+        }
+
+        public Proveedor(int id, string telefono, string direccion, int descuento)
+        {
+            Id = id;
+            Telefono = telefono;
+            Direccion = direccion;
+            Descuento = descuento;
+        }
     }
 }
