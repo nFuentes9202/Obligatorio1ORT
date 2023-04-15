f39179538c421833bee1844c5491b9380f8644c4
Nahuel Fuentes <NF288010@fi365.ort.edu.uy>
2023-04-15 18:11:15 -0300

    clase actividad agregada

---
+++ Obligatorio1/Actividad.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio1
+{
+    public abstract class Actividad
+    {
+        public static int UltimoId { get; set; }
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public int CantMaxPersonas { get; set; }
+        public int EdadMinima { get; set; }
+        public double CostoDolares { get; set; }
+        public int LugaresDisponibles { get; set; }
+
+        public Actividad()
+        {
+
+        }
+
+        public Actividad(string nombre, string descripcion, DateTime fecha, int cantMaxPersonas, int edadMinima, double costoDolares, int lugaresDisponibles)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Fecha = fecha;
+            CantMaxPersonas = cantMaxPersonas;
+            EdadMinima = edadMinima;
+            CostoDolares = costoDolares;
+            LugaresDisponibles = lugaresDisponibles;
+        }
+        public abstract void EsValido();
+    }
+}
