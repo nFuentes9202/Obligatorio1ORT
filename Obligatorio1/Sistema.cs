f812b562eff5847316b5299cac308a40ec9ef0c5
Nahuel Fuentes <NF288010@fi365.ort.edu.uy>
2023-04-21 11:37:16 -0300

    Implementación de la clase sistema con singleton.

---
+++ Obligatorio1/Sistema.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio1
+{
+    public class Sistema
+    {
+        private static Sistema _instance = null;
+        private Sistema()
+        {
+        }
+        public static Sistema GetInstancia()
+        {
+            if (_instance == null)
+            {
+                _instance = new Sistema();
+            }
+            return _instance;
+        }
+        List<Usuario> _usuarios = new List<Usuario>();
+        List<Actividad> _actividades = new List<Actividad>();
+        List<Proveedor> _proveedores = new List<Proveedor>();
+
+        public List<Usuario> GetUsuarios() {
+            return _usuarios;
+        }
+        public List<Actividad> GetActividades() { return _actividades; }
+        public List<Proveedor> GetProveedor() { return _proveedores; }
+
+        public void AltaUsuario(Usuario usu)
+        {
+            try
+            {
+                //TODO
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public void AltaActividad(Actividad act)
+        {
+            try
+            {
+                //TODO
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public void AltaProveedor(Proveedor prov)
+        {
+            try
+            {
+                //TODO
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
