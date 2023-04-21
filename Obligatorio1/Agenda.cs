6a29d08b25d8bdffb52f09bd73b4729d40a61d79
Nahuel Fuentes <NF288010@fi365.ort.edu.uy>
2023-04-21 11:44:34 -0300

    Implementación de la clase Agenda.

---
+++ Obligatorio1/Agenda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio1
+{
+    public class Agenda
+    {
+        public DateTime FechaCreacion { get; set; }
+        public string Estado { get; set; }
+        public Huesped Huesped { get; set; }
+        public Actividad Actividad { get; set; }
+        public Agenda() { }
+
+        public Agenda(string estado, Huesped huesped, Actividad actividad)
+        {
+            FechaCreacion = DateTime.Now;
+            Estado = estado;
+            Huesped = huesped;
+            Actividad = actividad;
+        }
+    }
+}
