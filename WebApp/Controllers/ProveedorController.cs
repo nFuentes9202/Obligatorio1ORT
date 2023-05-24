20be7f32abd2806a2bf979990c844bfde56dcce7
Richard Blank Pucholo <RB304390@fi365.ort.edu.uy>
2023-05-24 11:05:08 -0300

    Se agrega webapp

---
+++ WebApp/Controllers/ProveedorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
