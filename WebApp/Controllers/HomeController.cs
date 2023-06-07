bdab2329910e437c318bcc22adf3f6af41e76839
Richard Blank Pucholo <RB304390@fi365.ort.edu.uy>
2023-06-07 10:54:34 -0300

    Agregando vistas

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace WebApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Login()
         {
             return View();
         }
