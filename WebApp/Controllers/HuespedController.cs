a64a7d9d92d327f17724fca2eba92702c7446f19
Nahuel Fuentes <NF288010@fi365.ort.edu.uy>
2023-06-14 11:58:54 -0300

    Se hizo la lógica del layout para los distintos usuarios. Falta implementacion de controllers y vistas completas.

---
+++ WebApp/Controllers/HuespedController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Obligatorio1;
+
+namespace WebApp.Controllers
+{
+    public class HuespedController : Controller
+    {
+        Sistema s = Sistema.GetInstancia();
+        public IActionResult Registro()
+        {
+            return View();
+        }
+        
+    }
+}
