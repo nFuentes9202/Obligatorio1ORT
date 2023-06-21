d4ae3a4a6a9e2518c8df11b7bd7f330846532a30
Nahuel Fuentes <NF288010@fi365.ort.edu.uy>
2023-06-21 08:46:33 -0300

    Se agrego validacion js en el registro.

---
+++ WebApp/wwwroot/js/Usuario/RegistroHuesped.js
@@ -0,0 +1,17 @@
+﻿
+document.querySelector("#formRegistro").addEventListener("submit", validarDatos);
+
+function validarDatos(evt) {
+    evt.preventDefault();
+    let nombre = document.querySelector("#Nombre").value;
+    let apellido = document.querySelector("#Apellido").value;
+    let email = document.querySelector("#Email").value;
+    let contra = document.querySelector("#Contrasenia").value;
+    let nroDocumento = document.querySelector("#NumeroDocumento").value;
+    let habitacion = document.querySelector("#nroHabitacion").value;
+    if (nombre != "" && apellido != "" && email != "" && contra != "" && nroDocumento != "" && habitacion != "") {
+        this.submit();
+    } else {
+        document.querySelector("#pRegistro").innerHTML = "Verifique los datos";
+    }
+}
